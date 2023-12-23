c4bb1f6047876256c062e5da0067d06a0cc4d7e3
Hitesh IT <hiteshkumarnayak796@gmail.com>
2023-12-23 16:24:45 +0530

    db.txt file added

--- user.java
+++ user.java
@@ -5,6 +5,9 @@ public static void main (String [] args){
     System.out.println("Hello GITHUB");
 
     Sysetem.out.println("Hello cleint");
+
+    
+    Sysetem.out.println("Removed from Staged area to unstaged Area");
 }
 
 }
